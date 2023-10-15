affaeb7c7bc287df3189cbdfb54eff28e854cc5f
lucas richards dev <lucasrichardsdev@gmail.com>
2023-10-15 15:00:52 -0700

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,23 @@
+
+
 const Home = (props) => {
 
+	const styleHome = {
+		backgroundImage : 'url(investment.jpeg) ',
+		backgroundRepeat : 'no-repeat',
+		backgroundSize: 'cover',
+		padding: '150px',
+		height: '900px',
+		color: 'white'
+		
+	}
+
 	return (
-		<div className="m-5">
-			<h2>Home Page of the Trading App</h2>
+		<div style={styleHome}>
+			<h2>Trade with confidence on the world's leading social trading platform</h2>
+			<p>
+			Join millions who've already discovered smarter investing by automatically copying the leading traders in our community, or get copied yourself to earn a second income
+			</p>
 		</div>
 	)
 }
